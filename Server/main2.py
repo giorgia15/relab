cd2c130e42926feb081336f6b01c28fb45c0009a
giorgia15 <bozzetti.giorgia@iisgalvanimi.edu.it>
2021-11-17 12:25:13 +0000

    fine relab 2.0

--- Server/main2.py
+++ Server/main2.py
@@ -22,12 +22,23 @@ def index():
     return "Hello world!"
 
 # Questa route effettua una find() su tutto il DB (si limita ai primi 100 risultati)
-@app.route('/addresses', methods=['GET'])
+"""@app.route('/CI_VETTORE', methods=['GET'])
 def get_all_addresses():
     mil4326WKT = mongo.db.MilWKT4326
     output = []
     for s in mil4326WKT.find().limit(100):
-        output.append(s['INDIRIZZO'])
+        output.append(s['INDIRIZZO']+ '|' + s['CI_VETTORE'] )
+    return jsonify({'result': output})"""
+
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore(foglio):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "FOGLIO" : foglio
+    }
+    for s in mil4326WKT.find(query).limit(100):
+        output.append(s['CI_VETTORE'])
     return jsonify({'result': output})
 
 # Checks to see if the name of the package is the run as the main package.
